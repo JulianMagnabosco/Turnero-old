72164d0e83b44964f638084181121c483c09c036
Rai <julian.magnabosco@gmail.com>
2021-08-22 00:05:00 -0300

    agregue un fondito verde

--- contador.py
+++ contador.py
@@ -1,10 +1,12 @@
 import pygame,sys
+
+from pygame import surface
 flags = pygame.RESIZABLE
 screen = pygame.display.set_mode((500,500), flags)
 pygame.font.init()
 font1 = pygame.font.SysFont("Arial", 100)
 font2 = pygame.font.SysFont("Arial", 50)
-colorF = 0,200,0
+colorF = 255,255,255
 
 cola = list((1,2,3))
 
@@ -21,18 +23,18 @@ class Button :
     def change(self, bg="black"):
         """Change the text when you click"""
         self.text = self.font.render(self.textRaw, 1, pygame.Color("White"))
-        self.size = self.text.get_size()
+        self.size = (self.text.get_size()[0]+10,self.text.get_size()[1]+10)
         self.surface = pygame.Surface(self.size)
         self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(0, 0, self.size[0], self.size[1])
+        self.surface.blit(self.text, pygame.Rect(5,5,5,5))
+        self.rect = pygame.Rect(self.x/100*screen.get_size()[0], self.y/100*screen.get_size()[1], self.size[0], self.size[1])
 
     def show(self):
         if self.pressed:
             self.change(self.bg)
         else:
             self.change()
-        screen.blit(self.surface, (self.x/100*screen.get_size()[0], self.y/100*screen.get_size()[1]))
+        screen.blit(self.surface, (self.rect[0], self.rect[1]))
 
     def click(self, posX,posY):
         self.pressed = False
@@ -53,16 +55,19 @@ def resetTicket ():
     cola.clear()
     cola.append(1)
 
-def show():
-    # print(cola)
+def draw():
     render = font1.render(str(cola[0]), 1, colorF)
-    screen.blit(render, (20,20))
+    rectFont = (screen.get_size()[0]/4,35,render.get_size()[0],render.get_size()[1]-30)
+    pygame.draw.rect(screen, (0,255,0), rectFont, border_radius=5)
+    screen.blit(render, (screen.get_size()[0]/4,20))
     i = 0
     for n in cola:
         if n == cola[0]:
             continue
         render = font2.render(str(n), 1, colorF)
-        screen.blit(render, (20,120+i*50))
+        rectFont = (screen.get_size()[0]/4+15,125+i*50,render.get_size()[0],render.get_size()[1]-10)
+        pygame.draw.rect(screen, (0,255,0), rectFont, border_radius=5)
+        screen.blit(render, (screen.get_size()[0]/4+15,120+i*50))
         i += 1
 
 posButtons = 90
@@ -77,11 +82,8 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 posX,posY = pygame.mouse.get_pos()
-        # if event.type == pygame.MOUSEBUTTONUP:
-        #     posX = -1
-        #     posY = -1
     screen.fill((255,255,255))
-    show()
+    draw()
     pressedAdd = buttonAdd.click(posX,posY)
     pressedDel = buttonDel.click(posX,posY)
     pressedRes = buttonRes.click(posX,posY)
