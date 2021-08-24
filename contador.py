93e1e5b607b2184ff9605bd836e4cfa05770aa87
JM <julian.magnabosco@gmail.com>
2021-08-24 17:32:35 -0300

    otros toques y reorganizacion

--- contador.py
+++ contador.py
@@ -1,27 +1,25 @@
 import pygame,sys
 
 from pygame import surface
+import pygame.freetype
 flags = pygame.RESIZABLE
 screen = pygame.display.set_mode((1024,720), flags)
 pygame.font.init()
+pygame.freetype.init()
 font1 = pygame.font.SysFont("Arial", 100)
-font2 = pygame.font.SysFont("Arial", 50)
+font2 = pygame.freetype.SysFont("Segoe UI Emoji", 50)
 colorBF = 0,200,255
 
 cola = list((1,2,3))
 
 #clases
-class Button :
-    def __init__(self, text,  pos, action, bg="yellow"):
+class Button:
+    def __init__(self, text, action, bg="yellow"):
         self.font = font2
         self.textRaw = text
         self.text = self.font.render(self.textRaw, 1, pygame.Color("White"))
-        self.size = (self.text.get_size()[0]+10,self.text.get_size()[1]+10)
+        self.size = (self.text.get_sized_glyph_height()+10,self.text.get_sized_height()+10)
         self.surface = pygame.Surface(self.size)
-        # self.x = float(pos[0]-self.size[0]/screen.get_size()[0]*50)
-        # self.y = float(pos[1]-self.size[1]/screen.get_size()[1]*50)
-        self.x = pos[0]
-        self.y = pos[1]
         self.action = action
         self.pressed = False
         self.bg = bg
@@ -31,15 +29,15 @@ class Button :
         """Change the text when you click"""
         self.surface.fill(bg)
         self.surface.blit(self.text, pygame.Rect(5,5,5,5))
-        self.rect = pygame.Rect(self.x/100.0*screen.get_size()[0], self.y/100.0*screen.get_size()[1], self.size[0], self.size[1])
 
-    def show(self):
+    def show(self,  x,y):
+        self.rect = pygame.Rect(x-  self.size[0]/2, y-  self.size[1]/2, self.size[0], self.size[1])
         if self.pressed:
             self.change(self.bg)
             self.pressed = False
         else:
             self.change()
-        screen.blit(self.surface, (self.rect[0] - self.rect[2]/2, self.rect[1] - self.rect[3]/2))
+        screen.blit(self.surface, (x - self.rect[2]/2, y - self.rect[3]/2))
 
     def click(self):
         self.pressed = False
@@ -53,9 +51,10 @@ class Button :
 def addTicket ():
     cola.append(cola[-1]+1)
 
-def delTicket ():
+def nextTicket ():
     if len(cola) > 1:
         cola.pop(0)
+    
 
 def resetTicket ():
     cola.clear()
@@ -69,17 +68,17 @@ def draw():
     render = font1.render(str(len(cola)), 1, "white")
     screen.blit(render, (screen.get_size()[0] / 2 - render.get_size()[0] / 2,0))
 
-    buttonAdd.show()
-    buttonDel.show()
-    buttonRes.show()
-
+    posButton = posTop
+    for button in buttons:
+        button.show(screen.get_size()[0] * 0.75, button.size[1]/2 + posButton+5)
+        posButton += button.size[1] + 5
     
     render = font1.render(str(cola[0]), 1, "white")
     rectFont = (5, posTop + 5, screen.get_size()[0] / 2 - 5, render.get_size()[1])
     pygame.draw.rect(screen, colorBF, rectFont, border_radius=5)
     screen.blit(render, (screen.get_size()[0] / 4 - render.get_size()[0] / 2, posTop + 5))
     posRender = posTop + render.get_size()[1]+5
-    i = 0
+    
     for n in cola:
         if n == cola[0]:
             continue
@@ -88,22 +87,19 @@ def draw():
         pygame.draw.rect(screen, colorBF, rectFont, border_radius=5)
         screen.blit(render, (screen.get_size()[0] / 4 - render.get_size()[0] / 2,posRender+5))
         posRender = posRender + render.get_size()[1]+5
-        i += 1
 
-posButtons = 75
-buttonAdd = Button("addTicket",(posButtons,10),addTicket)
-buttonDel = Button("delTurn",(posButtons,20),delTicket)
-buttonRes = Button("resetTicket",(posButtons,30),resetTicket)
+buttons = {
+    Button("asdasd",addTicket),
+    Button("Llamar y quitar",nextTicket),
+    Button("Resetear todo",resetTicket)
+}
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 buttonAdd.click()
-                buttonDel.click()
+                buttonNext.click()
                 buttonRes.click()
-        # if event.type == pygame.MOUSEBUTTONUP:
-        #     posX = -1
-        #     posY = -1
     draw()
     pygame.display.flip()
