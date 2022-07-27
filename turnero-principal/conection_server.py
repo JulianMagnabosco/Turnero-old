6c27d2f041ce11f3ea9c99a38185dd2ee3b8e957
JM <julian.magnabosco@gmail.com>
2022-07-27 20:16:51 -0300

    terminado 2

--- turnero-principal/conection_server.py
+++ turnero-principal/conection_server.py
@@ -1,6 +1,5 @@
 import socket,threading
-import sys
-from turtle import update
+
 
 class ConectionServer:
     def __init__(self, update, exit):
@@ -70,19 +69,3 @@ class ConectionServer:
             self.alive = False
             self.my_socket.close()
                 
-
-class ConectionClient:
-    def __init__(self,address,port):
-        self.nickname = "local"
-        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.address = address# "127.0.1.1"
-        self.port = port
-        self.my_socket.connect((self.address, self.port))
-        self.data = self.my_socket.recv(1024).decode()
-        self.my_socket.send(f'@{self.nickname}'.encode())
-
-    def send(self,message):
-        self.my_socket.send(message.encode())
-        return self.my_socket.recv(1024).decode()
-        
-
